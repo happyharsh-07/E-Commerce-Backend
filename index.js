20c667ad5f301b09bb997724b1c0cae6e47836fb
Harsh V U Mishra <happyharsh2403@gmail.com>
2026-06-11 16:45:36 +0530

    feat: add image upload functionality using multer

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app=express();
 const cors=require("cors");
 app.use(express.json());
 app.use(cors());
+const multer=require("multer");
+const path=require("path");
 
 const mongoose=require("mongoose");
 mongoose.connect("mongodb+srv://happyharsh-07:<232107?Harsh>@cluster0.ew5vhen.mongodb.net/e-commerce");
@@ -18,6 +20,25 @@ mongoose.connect("mongodb+srv://happyharsh-07:<232107?Harsh>@cluster0.ew5vhen.mo
         old_price:{type:Number,required:true},
         date:{type:Date,default:Date.now},
         available:{type:Boolean,default:true},
+    });
+
+
+
+    const storage=multer.diskStorage({
+        destination:'./upload/images',
+        filename:(req,file,cb)=>{
+            return cb(null,`${file.fieldname}_${Date.now()} ${path.extname(file.originalname)}`);
+        }
+    });
+
+    const upload=multer({storage:storage});
+    app.use('/images',express.static('upload/images'));
+
+    app.post("/upload",upload.single('product'),(req,res)=>{
+        res.json({
+            success:1,
+            image_url:`http://localhost:${port}/images/${req.file.filename}`
+        });
     });
 
 
