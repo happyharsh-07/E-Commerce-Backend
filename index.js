861ee92ef50788848009253e94bfe6bc8aecfff7
Harsh V U Mishra <happyharsh2403@gmail.com>
2026-06-11 16:29:37 +0530

    feat: connect MongoDB and create Product schema

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const app=express();
 const cors=require("cors");
 app.use(express.json());
 app.use(cors());
-// Ek basic route check karne ke liye
 
+const mongoose=require("mongoose");
+mongoose.connect("mongodb+srv://happyharsh-07:<232107?Harsh>@cluster0.ew5vhen.mongodb.net/e-commerce");
+
+    //Schema for creating products
+    const Product=mongoose.model("Product",{
+        id:{type:Number,required:true},
+        name:{type:String,required:true},
+        image:{type:String,required:true},
+        category:{type:String,required:true},
+        new_price:{type:Number,required:true},
+        old_price:{type:Number,required:true},
+        date:{type:Date,default:Date.now},
+        available:{type:Boolean,default:true},
+    });
+
+
+//Ek basic route check karne ke liye
 app.get("/",(req,res)=>{
     res.send("EXPRESS App is running");;
 });
